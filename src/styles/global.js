f1d96455bdb6f2e8df1061b9e9fe428728709e83
gabriel <gabrielimabelo@gmail.com>
2024-02-18 11:35:03 +0000

    global style correction

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,1 +1,18 @@
-e
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.DARK[400]};
+  }
+`;
+
