7756dc45c130dd36f3241bf8f70fdc825a465561
gabriel <gabrielimabelo@gmail.com>
2024-02-23 01:24:35 +0000

    config addingredients

--- src/styles/global.js
+++ src/styles/global.js
@@ -59,5 +59,17 @@ export default createGlobalStyle`
   button:hover, a:hover {
     filter: brightness(.9);
   }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
 `;
 
