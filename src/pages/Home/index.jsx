bb508a68ae55f62c479a143230c32decb142de54
gabriel <gabrielimabelo@gmail.com>
2024-02-25 03:23:13 +0000

    mobile carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
-import { Container } from './styles';
+import { Container, Section, Banner } from './styles';
+import { motion } from 'framer-motion';
+import { useEffect, useState, useRef } from 'react';
 
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
@@ -8,28 +10,126 @@ import foots100 from '../../assets/foots-100.svg';
 import foots200 from '../../assets/foots-200.svg';
 
 export function Home() {
+  const carousel = useRef();
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth);
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
+  }, []);
+  
   return (
     <Container>
       <Header />
-      <main>
-      <div>
+      <Banner>
         <picture>
-          <source media="(max-width: 640px)" srcSet={foots100} />
+        <source media="(max-width: 640px)" srcSet={foots100} />
           <img src={foots200} alt="" />
         </picture>
         <div>
           <h2>Sabores inigualáveis</h2>
           <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
         </div>
-      </div>
-        <Card
-          dish={{
-            image: 'https://github.com/gabrielbeli',
-            name: 'Torradas de Parma',
-            description: 'Presunto de parma e rucula em um  pão com fermentação natural',
-            price: '25,97'
-          }}
-        />
+      </Banner>
+      <main>
+        <Section>
+          <h2>Pratos principais</h2>
+
+          <motion.div
+            ref={carousel}
+            className="carousel"
+            whileTap={{ cursor: 'grabbing' }}
+           >
+            <motion.div
+              className="inner"
+              drag="X"
+              dragConstraints={{ right: 0, left: -width }}
+              initial={{ x: 100 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 0.8 }}
+             >            
+             <motion.div className="item">
+              <Card
+               dish={{
+                image: 'https://github.com/gabrielbeli',
+                name: 'Torradas de Parma',
+                description: 'Presunto de parma e rucula em um pão com fermentação natural',
+                price: '25,97'
+               }}
+              />
+             </motion.div>
+             <motion.div className="item">
+              <Card
+               dish={{
+                image: 'https://github.com/gabrielbeli',
+                name: 'Torradas de Parma',
+                description: 'Presunto de parma e rucula em um pão com fermentação natural',
+                price: '25,97'
+               }}
+              />
+             </motion.div>
+             <motion.div className="item">
+              <Card
+               dish={{
+                image: 'https://github.com/gabrielbeli',
+                name: 'Torradas de Parma',
+                description: 'Presunto de parma e rucula em um pão com fermentação natural',
+                price: '25,97'
+               }}
+              />
+             </motion.div>            
+           </motion.div>
+         </motion.div>
+        </Section>
+
+        <Section>
+          <h2>Bebidas</h2>
+
+          <motion.div
+            ref={carousel}
+            className="carousel"
+            whileTap={{ cursor: 'grabbing' }}
+          >
+            <motion.div
+              className="inner"
+              drag="X"
+              dragConstraints={{ right: 0, left: -width }}
+              initial={{ x: 100 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 0.8 }}
+             >            
+             <motion.div className="item">
+              <Card
+               dish={{
+                image: 'https://github.com/gabrielbeli',
+                name: 'Torradas de Parma',
+                description: 'Presunto de parma e rucula em um pão com fermentação natural',
+                price: '25,97'
+               }}
+              />
+             </motion.div>
+             <motion.div className="item">
+              <Card
+               dish={{
+                image: 'https://github.com/gabrielbeli',
+                name: 'Torradas de Parma',
+                description: 'Presunto de parma e rucula em um pão com fermentação natural',
+                price: '25,97'
+               }}
+              />
+             </motion.div>
+             <motion.div className="item">
+              <Card
+               dish={{
+                image: 'https://github.com/gabrielbeli',
+                name: 'Torradas de Parma',
+                description: 'Presunto de parma e rucula em um pão com fermentação natural',
+                price: '25,97'
+               }}
+              />
+             </motion.div>            
+           </motion.div>
+          </motion.div>
+        </Section>
       </main>
       <Footer />
     </Container>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,93 +3,145 @@ import styled from "styled-components";
 export const Container = styled.div`
   min-height: 100vh;
   
+  overflow: hidden;
+  
+  display: grid;  
+  grid-template-areas: 
+    'header'
+    'banner'
+    'main'
+    'footer';
+  
+  grid-template-rows: 11.4rem 16.4rem auto 7.7rem;
+
   > header {
     position: sticky;
     z-index: 2;
     top: 0;
   }
-  
-  display: grid;  
-  grid-template-areas: 
-    'header'
-    'main'
-    'footer';
-  
-  grid-template-rows: 11.4rem auto 7.7rem;
 
   > main {
-    grid-area: main;
-    width: min(89%, 1122px);
+    grid-area: main;    
     margin: 0 auto;
-    padding: 4.4rem 0 2.5rem;
+    margin-top: 6.2rem;
+    padding: 0 0 2.5rem 0;
+    padding-left: 5.5vw;
+    overflow: hidden;
 
-    > div:first-child {
-      background: ${({ theme }) => theme.GRADIENTS[200]};
+    display: flex;
+    flex-direction: column;
+    gap: 2.4rem;
+  }
 
-      height: 12rem;
-      border-radius: 3px;
-      
-      display: flex;
-      justify-content: space-between;
-      position: relative;
+  @media (min-width: 641px) {
+    grid-template-rows: 9.3rem clamp(16.4rem, -18.2486rem + 54.0541vw, 42.rem) auto 7.7rem;
+  }
+`;
 
-      > picture > img {
-        position: absolute;
-        top: -3rem;
-        left: -3rem;
-        bottom: 0;
-      }
+export const Banner = styled.div`
+  height: 12rem;
+  width: min(89%, 1122px);
+  margin: 4.4rem auto 0;
+  grid-area: banner;
+  background: ${({ theme }) => theme.GRADIENTS[200]};
+  border-radius: 3px;
 
-      padding-right: clamp(0rem, -5.5556rem + 13.8889vw, 10rem);
-      > div {
-        font-family: 'Poppins', sans-serif;
-        width: 21.5rem;
-        padding-top: 3.6rem;
+  display: flex;
+  justify-content: flex-end;
 
-        > h2 {
-          font-size: 1.8rem;
-          font-weight: 600;
-          line-height: 2.5rem;
-          margin-bottom: 3px;
-        }
+  position: relative;
+  > picture {
+    position: absolute;
+    top: -3rem;
+    left: -3rem;
+    bottom: 0;
+  }
 
-        > p {
-          font-size: 1.2rem;
-          line-height: 1.7rem;
-        }
-      }
+  padding-right: clamp(0rem, -5.5556rem + 13.8889vw, 10rem);
+  > div {
+    font-family: 'Poppins', sans-serif;
+    width: 21.5rem;
+    padding-top: 3.6rem;
+
+    > h2 {
+      font-size: 1.8rem;
+      font-weight: 600;
+      line-height: 2.5rem;
+      margin-bottom: 3px;
+    }
+
+    > p {
+      font-size: 1.2rem;
+      line-height: 1.7rem;
     }
   }
 
   @media (max-width: 399px) {
-    > main > div:first-child {
-      justify-content: center;
-      align-items: center;
 
-      > picture > img {
-        display: none;
+    justify-content: center;
+    align-items: center;
+
+    > picture > img {
+      display: none;
+    }
+    > div {
+      padding-top: 0;
+    }
+  }
+  
+  @media (min-width: 641px) {
+    height: clamp(12rem, -6.657rem + 29.106vw, 26rem);
+    justify-content: space-between;
+    margin-top: clamp(4.4rem, -11.5917rem + 24.948vw, 16.4rem);
+    
+    > picture {
+      position: static;
+      overflow: hidden;
+      margin-top: -13%;
+      margin-left: -5%;
+      width: 58%;
+    }
+
+    > div {
+      width: 50%;
+      padding-top: clamp(4.4rem, -1.4636rem + 9.1476vw, 8.8rem);
+
+      > h2 {
+        text-align: center;
+        font-size: clamp(1.8rem, -1.1318rem + 4.5738vw, 4rem);
+        margin-bottom: clamp(0.3rem, -0.3663rem + 1.0395vw, 0.8rem);
+        line-height: clamp(2.5rem, -1.6312rem + 6.4449vw, 5.6rem);
       }
-      > div {
-        padding-top: 0;
+
+      > p {
+        font-size: clamp(1.2rem, 0.6669rem + 0.8316vw, 1.6rem);
+        line-height: 1.7rem;
+        text-align: center;
       }
     }
   }
+`;
 
-  @media (min-width: 641px) {
-    grid-template-rows: 9.3rem auto 7.7rem;
-    
-    > main {
-      padding-top: 16rem;
-      > div:first-child {
-        height: 26rem;
+export const Section = styled.section`
+  width: min(89vw, 1122px);
+  margin: 0 auto;
 
-        > picture {
-          background-color: red;
-          > img {
-            position: relative;
-            bottom: 0;
-          }
-        }
+  > h2 {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 1.8rem;
+    line-height: 2.5rem;
+    margin-bottom: 2.4rem;
+  }
+
+  .carousel {
+    cursor: grab;
+    overflow: hidden;
+    .inner {
+      display: flex;
+      gap: 1.6rem;
+
+      .item {        
       }
     }
   }
